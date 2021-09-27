b710fb59a150b31c330611b10168e365bf2c98e2
Omkar <omkartikare55@gmail.com>
2021-09-27 02:42:23 -0700

    front design improvement

--- wallpapers/urls.py
+++ wallpapers/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.start.as_view(), name="start_page"),
+
+    path('', views.Start, name="index"),
+    path("home/", views.HomePage.as_view(), name="home"),
+
+    path("home", views.WallCategory.as_view(), name="wallCategories"),
+    path('all-categories/', views.AllCategories.as_view(), name="start_page"),
+
     path('wallpapers/<slug:slug>', views.wallpapers, name="detail"),
     path('search', views.search_bar, name="searchbar"),
-    path("home/", views.HomePage.as_view(), name="home"),
+
+
 ]
--- wallpapers/views.py
+++ wallpapers/views.py
@@ -6,20 +6,93 @@ from django.urls import reverse
 from django.views import View
 
 
-class start(View):
+def Start(request):
+    index = Wallpapers.objects.all()[:1]
+    indexloop = Wallpapers.objects.all().order_by("-date")[1:5]
+    context = {
+        "index": index, "indexloop": indexloop
+    }
+    return render(request, "wallpapers/index.html", context)
+
+
+class HomePage(View):
+    def get(self, request):
+        filteredanimal = Wallpapers.objects.filter(
+            category__name__icontains="Animal").order_by("-date")[:1]
+
+        filtered3D = Wallpapers.objects.filter(
+            category__name__icontains="3D").order_by("-date")[:1]
+
+        filteredanime = Wallpapers.objects.filter(
+            category__name__icontains="Anime").order_by("-date")[:1]
+
+        filteredcity = Wallpapers.objects.filter(
+            category__name__icontains="City").order_by("-date")[:1]
+
+        filteredfantasy = Wallpapers.objects.filter(
+            category__name__icontains="Fantasy").order_by("-date")[:1]
+
+        filteredholidays = Wallpapers.objects.filter(
+            category__name__icontains="Holidays").order_by("-date")[:1]
+
+        filteredcars = Wallpapers.objects.filter(
+            category__name__icontains="Cars").order_by("-date")[:1]
+
+        filterednature = Wallpapers.objects.filter(
+            category__name__icontains="Nature").order_by("-date")[:1]
+
+        filteredart = Wallpapers.objects.filter(
+            category__name__icontains="Art").order_by("-date")[:1]
+
+        filteredspace = Wallpapers.objects.filter(
+            category__name__icontains="Space").order_by("-date")[:1]
+
+        context = {
+            "filteredanimal": filteredanimal,
+            "filtered3D": filtered3D,
+            "filteredanime": filteredanime,
+            "filteredcity": filteredcity,
+            "filteredcars": filteredcars,
+            "filteredfantasy": filteredfantasy,
+            "filteredholidays": filteredholidays,
+            "filterednature": filterednature,
+            "filteredcars": filteredcars,
+            "filteredspace": filteredspace,
+            "filteredart": filteredart
+
+        }
+
+        return render(request, "wallpapers/homepage.html", context)
+
+
+class WallCategory(View):
+    def get(self, request):
+        wallpapers = Wallpapers.objects.all()
+        category = request.GET.get('category')
+        wallpapers = Wallpapers.objects.filter(
+            category__name=category).order_by("-date")
+        context = {
+            "wallpapers": wallpapers,
+            "category": category,
+        }
+
+        return render(request, "wallpapers/wallCategories.html", context)
+
+
+class AllCategories(View):
     def get(self, request):
         categories = Category.objects.all()
         wallpapers = Wallpapers.objects.all()
         category = request.GET.get('category')
         if category == None:
-            wallpapers = Wallpapers.objects.all()
+            wallpapers = Wallpapers.objects.all().order_by("-date")[1:]
         else:
-            wallpapers = Wallpapers.objects.filter(category__name=category)
+            wallpapers = Wallpapers.objects.filter(
+                category__name=category).order_by("-date")
 
-        categories = Category.objects.all()
         context = {"categories": categories, "wallpapers": wallpapers,
                    "comment_form": CommentForm(), "comments": Comments.objects.all().order_by("-id")}
-        return render(request, "wallpapers/index.html", context)
+        return render(request, "wallpapers/all_categories.html", context)
 
     def post(self, request):
         comment_form = CommentForm(request.POST)
@@ -27,32 +100,13 @@ class start(View):
         wallpapers = Wallpapers.objects.all()
 
         if comment_form.is_valid():
-            comment = comment_form.save()
+            comment_form.save()
             return HttpResponseRedirect(reverse("start_page"))
         else:
             context = {"categories": categories, "wallpapers": wallpapers,
                        "comment_form": CommentForm(), "comments": Comments.objects.all().order_by("-id")}
             return render(request, 'start_page', context)
 
-
-# def index(request):
-#     comment_form = CommentForm(request.POST)
-#     category = request.GET.get('category')
-#     if category == None:
-#         wallpapers = Wallpapers.objects.all()
-#     else:
-#         wallpapers = Wallpapers.objects.filter(category__name=category)
-
-#     categories = Category.objects.all()
-
-#     if comment_form.is_valid():
-#         return HttpResponseRedirect(reverse("start_page"))
-
-#     context = {"categories": categories, "wallpapers": wallpapers,
-#                "comment_form": CommentForm()}
-#     return render(request, "wallpapers/index.html", context)
-
-#         comment = comment_form.save()
 
 def wallpapers(request, slug):
     wallpapers = Wallpapers.objects.get(slug=slug)
@@ -70,11 +124,3 @@ def search_bar(request):
         "result": result
     }
     return render(request, "wallpapers/search_bar.html", context)
-
-
-class HomePage(View):
-
-    def get(self, request):
-        new = Wallpapers.objects.filter(
-            category__name__icontains="3D")[:1]
-        return render(request, "wallpapers/homepage.html", {"new": new})
